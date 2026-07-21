e85da8de7b85fc502d423792ab6c2b65f339a793
Ubuntu <ubuntu@ip-172-31-9-188.ap-south-1.compute.internal>
2026-07-21 07:10:56 +0000

    terraform for EKS

---
+++ terraform/eks.tf
@@ -0,0 +1,30 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.37"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.31"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.small"]
+
+      min_size     = 2
+      desired_size = 2
+      max_size     = 3
+
+      capacity_type = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Project = "swimlane"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  default = "swimlane-eks"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
